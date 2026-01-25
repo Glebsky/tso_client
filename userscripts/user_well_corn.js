85335546444375e5e86517ec93c906fcaace3bc5
Hlib <lotamountofsoul@gmail.com>
2026-01-25 22:01:22 +0200

    ✨ feat(userscripts): Add build types and names for corn and well scripts
    - Added BUILD_TYPE and BUILD_NAMES constants for corn and well scripts.
    - Updated UIMap to include build-specific buttons and data.
    - Implemented logic to display correct buttons and counts based on the active tab.

--- userscripts/user_well_corn.js
+++ userscripts/user_well_corn.js
@@ -2,6 +2,13 @@ const WellCorn = (function () {
     const SCRIPT_CONST = {
         PREFIX: 'WC',
         NAME: 'Well Corn',
+        BUILD_TYPE: {well: 'well', corn:'corn'},
+        BUILD_NAMES: {
+            WELL: 'Well_03',
+            WELL_DEPLETED: 'MineDepletedDepositWater',
+            CORN:'Farmfield_03',
+            CORN_DEPLETED: 'MineDepletedDepositCorn',
+        }
     };
 
     const UIMap = {
@@ -10,6 +17,8 @@ const WellCorn = (function () {
             modalData: SCRIPT_CONST.PREFIX + '_WellCornModalData',
             cornBtn: SCRIPT_CONST.PREFIX + '_CornBtn',
             wellBtn: SCRIPT_CONST.PREFIX + '_WellBtn',
+            startBtn: SCRIPT_CONST.PREFIX + '_StartBtn',
+            stopBtn: SCRIPT_CONST.PREFIX + '_StopBtn',
             counts: SCRIPT_CONST.PREFIX + '_Counts'
         },
         classes: {}
@@ -28,6 +37,9 @@ const WellCorn = (function () {
             UIRenderer.renderBody();
             UIRenderer.renderFooter();
 
+            ActionService.bindEvents();
+            ActionService.update();
+
             $('.modal-header').css('padding-top', '20px');
             $('#' + UIMap.ids.modal + ':not(:visible)').modal({backdrop: "static"});
         } catch (e) {
@@ -45,49 +57,61 @@ const WellCorn = (function () {
     }
 
     const ActionService = (function() {
-        var activeTab = 'corn';
-        var isRunning = false;
-        var wellDepletedName = 'MineDepletedDepositWell';
 
-        function setActiveTab(tab) {
-            activeTab = tab;
+        function bindEvents() {
+            $('#' + UIMap.ids.cornBtn).click(function() { setActiveTab(SCRIPT_CONST.BUILD_NAMES.CORN); });
+            $('#' + UIMap.ids.wellBtn).click(function() { setActiveTab(SCRIPT_CONST.BUILD_NAMES.WELL); });
+            $('#' + UIMap.ids.startBtn).click(startBuilding);
+            $('#' + UIMap.ids.stopBtn).click(stopBuilding);
         }
 
-        function getActiveTab() {
-            return activeTab;
+        function setActiveTab(tab) {
+            var state = SettingsService.getState();
+            state.data.activeTab = tab;
+            SettingsService.setState(state);
+            SettingsService.saveSettings();
+            update();
+        }
+
+        function update() {
+            var countData = getCount();
+            UIRenderer.update(SettingsService.getActiveTab(), countData);
         }
 
         function getDepletedName() {
-            return activeTab === 'corn' ? 'MineDepletedDepositCorn' : wellDepletedName;
+            return SettingsService.getState().data.activeTab === SCRIPT_CONST.BUILD_NAMES.CORN ? SCRIPT_CONST.BUILD_NAMES.CORN_DEPLETED : SCRIPT_CONST.BUILD_NAMES.WELL_DEPLETED;
         }
 
         function getCount() {
             var name = getDepletedName();
             var count = 0;
             try {
-                if (game.zone.mStreetDataMap && game.zone.mStreetDataMap.GetNrFromName) {
-                    count = game.zone.mStreetDataMap.GetNrFromName(name);
+                if (game.zone.mStreetDataMap && game.zone.mStreetDataMap.getBuildingsByName_vector) {
+                    count = game.zone.mStreetDataMap.getBuildingsByName_vector(name).length;
                 }
             } catch(e) {
-                // ignore
+                debug(e);
             }
             return { count: count, name: name };
         }
 
         function startBuilding() {
-            isRunning = true;
-            debug("Start building " + activeTab);
+            var state = SettingsService.getState();
+            state.isRunning = true;
+            SettingsService.setState(state);
+            debug("Start building " + SettingsService.getActiveTab());
         }
 
         function stopBuilding() {
-            isRunning = false;
+            var state = SettingsService.getState();
+            state.isRunning = false;
+            SettingsService.setState(state);
             debug("Stop building");
         }
 
         return {
-            setActiveTab: setActiveTab,
-            getActiveTab: getActiveTab,
-            getCount: getCount,
+            bindEvents: bindEvents,
+            update: update,
             startBuilding: startBuilding,
             stopBuilding: stopBuilding
         };
@@ -99,37 +123,17 @@ const WellCorn = (function () {
             $modalData.empty();
 
             var container = $('<div>', {'class': 'container-fluid'});
-            
+
             // Row 1: Radio Buttons (Images)
             var row1 = $('<div>', {'class': 'row', 'style': 'text-align: center; margin-bottom: 15px;'});
             var col1 = $('<div>', {'class': 'col-xs-12'});
-            
-            var style = 'cursor: pointer; margin: 0 5px; transition: opacity 0.3s;';
-            
-            var btnCorn = $('<div>', {'id': UIMap.ids.cornBtn, 'style': 'display: inline-block;' + style})
-                .append(getImageTag('Farmfield_03', '24px'));
-            
-            var btnWell = $('<div>', {'id': UIMap.ids.wellBtn, 'style': 'display: inline-block;' + style})
-                .append(getImageTag('Well', '24px'));
 
-            function updateVisuals() {
-                var activeTab = ActionService.getActiveTab();
-                if (activeTab === 'corn') {
-                    btnCorn.css('opacity', '1');
-                    btnWell.css('opacity', '0.5');
-                } else {
-                    btnCorn.css('opacity', '0.5');
-                    btnWell.css('opacity', '1');
-                }
-                updateCounts();
-            }
-
-            btnCorn.click(function() { ActionService.setActiveTab('corn'); updateVisuals(); });
-            btnWell.click(function() { ActionService.setActiveTab('well'); updateVisuals(); });
+            var btnCorn = createIconButton(UIMap.ids.cornBtn, 'Farmfield_03','50px');
+            var btnWell = createIconButton(UIMap.ids.wellBtn, 'Well','50px');
 
             col1.append(btnCorn, btnWell);
             row1.append(col1);
-            
+
             // Row 2: Counts Info
             var row2 = $('<div>', {'class': 'row', 'style': 'text-align: center; margin-bottom: 15px;'});
             var col2 = $('<div>', {'class': 'col-xs-12', 'id': UIMap.ids.counts, 'style': 'font-weight: bold;'});
@@ -138,36 +142,53 @@ const WellCorn = (function () {
             // Row 3: Start/Stop Buttons
             var row3 = $('<div>', {'class': 'row', 'style': 'text-align: center;'});
             var col3 = $('<div>', {'class': 'col-xs-12'});
-            
-            var btnStart = $('<button>', {'class': 'btn btn-success', 'style': 'margin-right: 5px;'}).text('Start').click(ActionService.startBuilding);
-            var btnStop = $('<button>', {'class': 'btn btn-danger'}).text('Stop').click(ActionService.stopBuilding);
-            
+
+            var btnStart = createIconButton(UIMap.ids.startBtn, 'ButtonIconStart','35px');
+            var btnStop = createIconButton(UIMap.ids.stopBtn, 'ButtonIconStop','35px');
+
             col3.append(btnStart, btnStop);
             row3.append(col3);
 
             container.append(row1, row2, row3);
             $modalData.append(container);
-            
-            updateVisuals();
+        }
+
+        function createIconButton(id, iconName, extraStyle) {
+            var style = 'cursor: pointer; margin: 0 5px; transition: opacity 0.3s; display: inline-block;';
+            return $('<div>', {'id': id, 'style': style})
+                .append(getImageTag(iconName, extraStyle));
         }
 
         function renderFooter() {
             var $modal = $('#' + UIMap.ids.modal);
             var $footer = $modal.find('.modal-footer');
             $footer.empty();
-            
+
             var btnClose = $('<button>', {'class': 'btn btn-secondary', 'data-dismiss': 'modal'}).text(loca.GetText("LAB", "Close"));
             $footer.append(btnClose);
         }
 
-        function updateCounts() {
-            var data = ActionService.getCount();
-            $('#' + UIMap.ids.counts).html(loca.GetText("LAB", "Amount") + ': ' + data.count + ' (' + data.name + ')');
+        function update(activeTab, data) {
+            // Update Visuals (Opacity)
+            var btnCorn = $('#' + UIMap.ids.cornBtn);
+            var btnWell = $('#' + UIMap.ids.wellBtn);
+
+            if (activeTab === SCRIPT_CONST.BUILD_NAMES.CORN) {
+                btnCorn.css('opacity', '1');
+                btnWell.css('opacity', '0.5');
+            } else {
+                btnCorn.css('opacity', '0.5');
+                btnWell.css('opacity', '1');
+            }
+
+            // Update Counts
+            $('#' + UIMap.ids.counts).html(getImageTag(data.name, '50px') + ' ' + data.count);
         }
 
         return {
             renderBody: renderBody,
-            renderFooter: renderFooter
+            renderFooter: renderFooter,
+            update: update
         };
     })();
 
@@ -177,7 +198,10 @@ const WellCorn = (function () {
         function initStateData() {
             return {
                 modalInitialized: false,
-                data: {}
+                isRunning: false,
+                data: {
+                    activeTab: false
+                }
             }
         }
 
@@ -199,12 +223,16 @@ const WellCorn = (function () {
             STATE = state;
         }
 
+        function getActiveTab(){
+            return STATE.data.activeTab;
+        }
+
         return {
             getState: getState,
             setState: setState,
             loadSettings: loadSettings,
             saveSettings: saveSettings,
-            resetState: resetState,
+            getActiveTab: getActiveTab
         };
     })();
 
